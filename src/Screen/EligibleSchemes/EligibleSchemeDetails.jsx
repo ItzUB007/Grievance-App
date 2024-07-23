8cc97155aeb23de25fc7d53c52938d0df8db122f
uddeshyasama <uddeshya.ud@samaavesh.com>
2024-07-23 14:29:54 +0530

    updated-updateamember

--- src/Screen/EligibleSchemes/EligibleSchemeDetails.jsx
+++ src/Screen/EligibleSchemes/EligibleSchemeDetails.jsx
@@ -1,36 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView, StyleSheet, Linking, TouchableOpacity, Alert } from 'react-native';
-
-
-
-
+import { View, Text, ScrollView, StyleSheet, ActivityIndicator } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 const EligibleSchemeDetails = ({ route }) => {
-  const { schemeDetails } = route.params;
-
+  const { schemeDetails, schemeId } = route.params;
+  const [scheme, setScheme] = useState(schemeDetails);
+  const [loading, setLoading] = useState(!schemeDetails);
 
   useEffect(() => {
-  
-console.log(schemeDetails);
-  }, [schemeDetails]);
+    const fetchSchemeDetails = async () => {
+      if (!schemeDetails && schemeId) {
+        setLoading(true);
+        try {
+          const schemeDoc = await firestore().collection('Schemes').doc(schemeId).get();
+          if (schemeDoc.exists) {
+            setScheme(schemeDoc.data());
+          } else {
+            Alert.alert('Error', 'Scheme not found.');
+          }
+        } catch (error) {
+          console.error('Error fetching scheme details: ', error);
+          Alert.alert('Error', 'Failed to fetch scheme details.');
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
 
-  
+    fetchSchemeDetails();
+  }, [schemeDetails, schemeId]);
+
+  if (loading) {
+    return (
+      <View style={styles.loaderContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
 
   return (
     <ScrollView style={styles.container}>
-      {schemeDetails && (
+      {scheme && (
         <View style={styles.detailsContainer}>
-            <Text style={styles.detailText}>Scheme Name: {schemeDetails.Name}</Text>
-          <Text style={styles.detailText}>Application Method : {schemeDetails.ApplicationMethod}</Text>
-          <Text style={styles.detailText}>Govt Fee : {schemeDetails.GovtFee}</Text> 
-           <Text style={styles.detailText}>Description: {schemeDetails.Description}</Text>
-          <Text style={styles.detailText}>Eligibility {schemeDetails.Eligibility}</Text>
-          <Text style={styles.detailText}>Scheme Type {schemeDetails.SchemeType}</Text>
-          <Text style={styles.detailText}>benifitDescription {schemeDetails.benefitDescription}</Text>
-          <Text style={styles.detailText}>Process {schemeDetails.process}</Text>
+          <Text style={styles.detailText}>Scheme Name: {scheme.Name}</Text>
+          <Text style={styles.detailText}>Application Method: {scheme.ApplicationMethod}</Text>
+          <Text style={styles.detailText}>Govt Fee: {scheme.GovtFee}</Text>
+          <Text style={styles.detailText}>Description: {scheme.Description}</Text>
+          <Text style={styles.detailText}>Eligibility: {scheme.Eligibility}</Text>
+          <Text style={styles.detailText}>Scheme Type: {scheme.SchemeType}</Text>
+          <Text style={styles.detailText}>Benefit Description: {scheme.benefitDescription}</Text>
+          <Text style={styles.detailText}>Process: {scheme.process}</Text>
         </View>
       )}
-
     </ScrollView>
   );
 };
@@ -38,10 +59,10 @@ console.log(schemeDetails);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F0F0F0' // Consistent background with ViewStatusScreen
+    backgroundColor: '#F0F0F0',
   },
   detailsContainer: {
-    backgroundColor: 'white', // Card-like style for details
+    backgroundColor: 'white',
     padding: 20,
     margin: 10,
     borderRadius: 10,
@@ -54,12 +75,13 @@ const styles = StyleSheet.create({
   detailText: {
     fontSize: 16,
     marginBottom: 10,
-    color: '#333' // Dark color for text for better readability
+    color: '#333',
   },
-
- 
-
+  loaderContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 export default EligibleSchemeDetails;
-
--- src/Screen/UpdateaMemberScreen/UpdateaMember.jsx
+++ src/Screen/UpdateaMemberScreen/UpdateaMember.jsx
@@ -1,10 +1,119 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ActivityIndicator } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import { useAuth } from '../../contexts/AuthContext';
 
-export default function UpdateaMember() {
+export default function UpdateaMember({ navigation }) {
+  const { userData } = useAuth();
+  const [members, setMembers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchMembers();
+  }, []);
+
+  const fetchMembers = async () => {
+    try {
+      const membersRef = firestore().collection('Members');
+      const memberSnapshot = await membersRef.where('ProgramId', '==', userData.ProgramId).get();
+
+      const membersList = memberSnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+
+      setMembers(membersList);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching members: ', error);
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.loaderContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>UpdateaMember</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>All Members</Text>
+      {members.map((member, index) => (
+        <View key={index} style={styles.memberContainer}>
+          <Text style={styles.baseText}>Name: {member.name}</Text>
+          <Text style={styles.baseText}>Phone No: {member.phoneNumber}</Text>
+          <Text style={styles.baseText}>Eligible Schemes: </Text>
+          {member.eligibleSchemes && member.eligibleSchemes.map((scheme, index) => (
+            <TouchableOpacity 
+              key={index} 
+              style={styles.schemeItem}
+              onPress={() => navigation.navigate('EligibleSchemeDetails', { schemeId: scheme.id })}
+            >
+              <Text style={styles.schemeText}>Scheme Name : {scheme.name}</Text>
+              
+            </TouchableOpacity>
+          ))}
+          <Text style={styles.baseText}>Available Tickets : {member.TicketId.length} </Text>
+             
+        </View>
+      ))}
+       
+
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  memberContainer: {
+    marginBottom: 20,
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 5,
+    elevation: 1,
+  },
+  schemeItem: {
+    
+    marginVertical: 5,
+    borderRadius: 5,
+   
+  },
+  baseText: {
+    fontSize: 14,
+    color: '#555',
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 24,
+    color: '#333',
+  },
+  separator: {
+    width: '100%',
+    height: 1,
+    backgroundColor: '#ccc',
+    marginTop: 20,
+  },
+  loaderContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },  schemeText: {
+    fontSize: 14,
+    color: 'blue',
+    textAlign: 'center',
+  },
+});
